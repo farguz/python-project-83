347b37eb67cd31c1f8bf72758a3e727a40cf2989
aleksei z <farguzalexey@gmail.com>
2025-08-02 18:16:40 +0300

    create templates

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# temporary
+@app.route('/urls')
+def urls():
+    return render_template('urls.html')
+
+@app.route('/url/<int:id>')
+def url():
+    return render_template('url.html')
