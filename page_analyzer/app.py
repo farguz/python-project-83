8a8a08eb90bb63295ae1350d3dd038b988a11506
aleksei z <farguzalexey@gmail.com>
2025-08-05 14:04:35 +0300

    add normalize and validate url funcs

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -2,6 +2,7 @@ import os
 
 import psycopg2
 import validators
+import urllib.parse
 from dotenv import load_dotenv
 from flask import (
     Flask,
@@ -22,33 +23,77 @@ def connect_database():
     conn = psycopg2.connect(DATABASE_URL)
     return conn
 
+def validate_url(url: str) -> bool:
+    correct_url = validators.url(url)
+    correct_length = True if len(url) < 255 else False
+    # correct_length = validators.length(url, 0, 255) ПОЧЕМУ НЕ РАБОТАЕТ???
+    if correct_url is True and correct_length:
+        return True
+    return False
+
+def normalize_url(url: str) -> str:
+    normalized_url = url.lower().strip(' /')
+    # использовать urlparse???
+    return normalized_url
+
+def check_is_not_double(url: str) -> bool|int:
+    conn = connect_database()
+    sql = "SELECT * FROM urls WHERE name = (%s);"
+    with conn.cursor() as curs:
+        curs.execute(sql, (url, ))
+        conn.commit()
+        id = curs.fetchone()
+        conn.close()
+        if id is None:
+            return True
+        return id[0]
+
+def save_to_db(website):
+    pass
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html',
-                           url = '')
+                           url='')
 
 
 @app.route('/', methods=['POST'])
 def post_url():
     data = request.form.to_dict()
-    url = data['url']
-    correct_url = validators.url(url)
-    correct_length = True if len(url) < 255 else False
-    # correct_length = validators.length(url, 0, 255) ПОЧЕМУ НЕ РАБОТАЕТ???
-
-    if correct_url is True and correct_length:
+    url = normalize_url(data['url'])
+    correctness = validate_url(url)
+    doubleness = check_is_not_double(url)
+    if doubleness is not True:
+        flash("Страница уже существует", "info")
+        return redirect(url_for("get_url_info", id=doubleness))
+    if correctness and doubleness:
         conn = connect_database()
-        sql = "INSERT INTO urls (name) VALUES (%s);"
-
+        sql = "INSERT INTO urls (name) VALUES (%s) RETURNING id;"
         with conn.cursor() as curs:
-            curs.execute(sql, (data['url'], ))
+            curs.execute(sql, (url, ))
+            conn.commit()
+            id = curs.fetchone()[0]
+            conn.close()
         flash("Страница успешно добавлена", "success")
-        return redirect(url_for("index"))
+        return redirect(url_for("get_url_info", id=id))
     
     flash("Некорректный URL", "error")
     return render_template('index.html',
-                           url = url), 422
+                           url=url), 422
+
+
+@app.route('/urls/<int:id>', methods=['GET'])
+def get_url_info(id):
+    sql = 'SELECT id, name, created_at FROM urls WHERE urls.id = (%s);'
+    conn = connect_database()
+    with conn.cursor() as curs:
+        curs.execute(sql, (id, ))
+        conn.commit()
+        data = curs.fetchall()
+
+        # id, name, created_at = result[0], result[1], result[2]
+    return render_template('url_id.html',
+                           data=data)
 
 
 """@app.route('/urls', methods = ['GET'])
@@ -63,9 +108,7 @@ def urls_list():
 
 
 
-@app.route('/urls/<int:id>', methods = ['GET'])
-def get_url_info(id):
-    return render_template('url.html')
+
 
 def create_table(conn):
     sql = '''
