eb93f0b857badb9af94a099535183d669ff61dca
aleksei z <farguzalexey@gmail.com>
2025-08-05 17:09:55 +0300

    remove timestamp

--- page_analyzer/database.sql
+++ page_analyzer/database.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS urls (
         id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         name VARCHAR(255) NOT NULL,
-        created_at TIMESTAMP DEFAULT NOW()
+        created_at NOT NULL
     );
