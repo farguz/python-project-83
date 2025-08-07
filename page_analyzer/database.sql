1c2d35dd22e291e56b92edf922af63a0207b90d5
aleksei z <farguzalexey@gmail.com>
2025-08-07 12:42:11 +0300

    add new table url_checks

--- page_analyzer/database.sql
+++ page_analyzer/database.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS urls (
         id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-        name VARCHAR(255) NOT NULL,
+        name VARCHAR(255) NOT NULL UNIQUE,
         created_at TIMESTAMP NOT NULL
     );
 
 CREATE TABLE IF NOT EXISTS url_checks  (
         id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-        url_id BIGINT NOT NULL,
+        url_id BIGINT REFERENCES urls (id) NOT NULL,
         status_code INT,
         h1 VARCHAR(255),
         title VARCHAR(255),
